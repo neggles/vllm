cf769d23f5341c3f2c19c76bdf2efadd400edcc4
Hongxia Yang <hongxyan@amd.com>
2024-06-13 21:12:38 +0000

    revert skipIfROCm to see whether it fails again with upstream update

--- tests/quantization/test_compressed_tensors.py
+++ tests/quantization/test_compressed_tensors.py
@@ -4,7 +4,6 @@ Run `pytest tests/quantization/test_compressed_tensors.py`.
 """
 
 import torch
-from torch.testing._internal.common_utils import skipIfRocm
 
 from vllm import SamplingParams
 from vllm.model_executor.layers.quantization.compressed_tensors.compressed_tensors import (  # noqa: E501
@@ -12,7 +11,6 @@ from vllm.model_executor.layers.quantization.compressed_tensors.compressed_tenso
     CompressedTensorsW8A8StaticTensor)
 
 
-@skipIfRocm
 def test_compressed_tensors_w8a8_static_setup(vllm_runner):
     model_path = "nm-testing/tinyllama-oneshot-w8a8-static-v2"
     with vllm_runner(model_path, enforce_eager=True) as llm:
@@ -42,7 +40,6 @@ def test_compressed_tensors_w8a8_static_setup(vllm_runner):
         assert qkv_proj.input_scale.dtype is torch.float32
 
 
-@skipIfRocm
 def test_compressed_tensors_no_enforce_eager(vllm_runner):
     model_path = "nm-testing/tinyllama-oneshot-w8a8-static-v2"
     with vllm_runner(model_path) as llm:
@@ -51,7 +48,6 @@ def test_compressed_tensors_no_enforce_eager(vllm_runner):
         assert output
 
 
-@skipIfRocm
 def test_compressed_tensors_w8a8_dynanmic_per_token(vllm_runner):
     model_path = "nm-testing/tinyllama-oneshot-w8a8-dynamic-token-v2"
     with vllm_runner(model_path, enforce_eager=True,
